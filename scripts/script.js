2c7959ce276a668caac8266f27705e551c421c0f
antwan-twan <anthonie.meijers@hva.nl>
2021-11-11 14:40:40 +0100

    added nav and improved popup

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,19 @@ function switchMode() {
     document.documentElement.classList.remove("darkMode");
   }
 };
+
+
+
+// popup
+const popup = document.querySelector(".popup");
+const close = document.querySelector(".close");
+
+// window.onload = function(){
+//     setTimeout(function(){
+//         popup.style.display = "block";
+//     }, 2000) //2s vertraging
+// }
+
+close.addEventListener("click",() =>{
+    popup.style.display = "none";
+})
